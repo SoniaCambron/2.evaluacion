a4703a5af2806fb2e656722843978525fe7538b9
SoniaCambron <55531221+SoniaCambron@users.noreply.github.com>
2019-11-28 09:32:08 +0100

    Add files via upload

---
+++ hola.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+int main(){
+    //Definicion de variables
+    char salir;
+    char respuesta;
+    std::cout <<"Hola mundo, mi primer programa.";
+    std::cout<<" Eres guapo? (s/n)";
+    std::cin>>respuesta;
+    
+    if(respuesta=='s'){
+         std::cout<<"Hola guapo";
+    }
+    else{
+         std::cout<<"Hola feo";
+    }
+    std::cin>> salir;
+}
